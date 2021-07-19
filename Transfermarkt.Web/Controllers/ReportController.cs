bd5dc8a6d9691cdddb812d12bbe6f9a5600ed4a4
abedin123 <abedin.halilovic@edu.fit.ba>
2021-07-19 13:26:22 +0200

    Full push

---
+++ Transfermarkt.Web/Controllers/ReportController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AspNetCore.Reporting;
+using AspNetCore.ReportingServices.ReportProcessing.ReportObjectModel;
+using Microsoft.AspNetCore.Mvc;
+using temp;
+using Transfermarkt.Web.Data;
+
+namespace Transfermarkt.Web.Controllers
+{
+    public class ReportController : Controller
+    {
+        private AppDbContext _context;
+
+        public ReportController(AppDbContext db)
+        {
+            _context = db;
+        }
+        public List<IgraciVM> GetPlayers (AppDbContext db)
+        {
+            List<IgraciVM> data = _context.Players.Select(a => new IgraciVM
+            {
+                FirstName= a.FirstName,
+                LastName=a.LastName,
+                MiddleName=a.MiddleName,
+                height=a.Height
+            }).ToList();
+
+            return data;
+        }
+        public IActionResult Index()
+        {
+
+            LocalReport localreport = new LocalReport("Reporti/Report1.rdlc");
+            List<IgraciVM> data = GetPlayers(_context);
+            localreport.AddDataSource("DataSet1", data);
+
+            ReportResult result = localreport.Execute(RenderType.Pdf);
+
+            return File(result.MainStream, "application/pdf");
+        }
+    }
+}
---
+++ temp/IgraciVM.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace temp
+{
+    public class IgraciVM
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string MiddleName { get; set; }
+        public int height { get; set; }
+        public string brithplace { get; set; }
+
+        public static List<IgraciVM> Get()
+        {
+            return new List<IgraciVM> { };
+        }
+    }
+}
